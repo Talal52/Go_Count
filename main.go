4116159ca9944f9990248c0da5da8b9fad2d44ad
talal <talalafzal57@gmail.com>
2025-04-08 19:30:00 +0500

    count words, lines, punctuations, spaces

---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
+type Count struct {
+	Lines        int
+	Words        int
+	Vowels       int
+	Punctuations int
+	Spaces       int
+}
+
+func main() {
+	start := time.Now()
+
+	content, err := os.ReadFile("file.txt")
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		return
+	}
+
+	text := string(content)
+	var count Count
+
+	for _, char := range text {
+		switch {
+		case char == ' ':
+			count.Spaces++
+		case char == '\t' || char == '\r':
+			count.Words++
+		case char == 'A' || char == 'E' || char == 'I' || char == 'O' || char == 'U' || char == 'a' || char == 'e' || char == 'i' || char == 'o' || char == 'u':
+			count.Vowels++
+		case char == '.' || char == '!' || char == '?' || char == ',' || char == ':' || char == ';' || char == '(' || char == ')' || char == '[' || char == ']' || char == '{' || char == '}':
+			count.Punctuations++
+			if char == ',' {
+				count.Words++
+			}
+			count.Words++
+		case char == '\n':
+			count.Lines++
+
+		}
+	}
+	count.Words = count.Lines + count.Spaces // Assuming words are separated by spaces and new lines
+	count.Lines++                            // Increment line count for the last line if it doesn't end with a newline
+	// Print results
+	fmt.Println("Number of lines:", count.Lines+1) // +1 to count the last line if no newline at the end
+	fmt.Println("Number of words:", count.Words)
+	fmt.Println("Number of vowels:", count.Vowels)
+	fmt.Println("Number of punctuations:", count.Punctuations)
+	fmt.Println("Number of spaces:", count.Spaces)
+
+	// Execution time
+	fmt.Println("Execution time:", time.Since(start))
+}
