09202f97f7ebbe223d0808a8c8449e905d630c98
talal <talalafzal57@gmail.com>
2025-04-11 17:13:07 +0500

    gin frameword

--- main.go
+++ main.go
@@ -1,21 +1,38 @@
 package main
 
 import (
-    "github.com/Talal52/Go_Count/utils"
-    "fmt"
-    "time"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/Talal52/Go_Count/utils"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-    start := time.Now()
+	start := time.Now()
+	r := gin.Default()
 
-    lines, words, vowels, punctuations, spaces, err := utils.ReadFile("file.txt")
-    if err != nil {
-        fmt.Println("Error reading file:", err)
-        return
-    }
+	// POST route
+	r.POST("/readFile", func(c *gin.Context) {
+		lines, words, vowels, punctuations, spaces, err := utils.ReadFile("file.txt")
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error reading file"})
+			return
+		}
 
-    // Use the returned values
-    fmt.Printf("Lines: %d, Words: %d, Vowels: %d, Punctuations: %d, Spaces: %d\n", lines, words, vowels, punctuations, spaces)
-    fmt.Printf("Execution time: %s\n", time.Since(start))
+		//results as a JSON response
+		c.JSON(http.StatusOK, gin.H{
+			"lines":        lines,
+			"words":        words,
+			"vowels":       vowels,
+			"punctuations": punctuations,
+			"spaces":       spaces,
+		})
+	})
+
+	// Start server
+	r.Run()
+
+	fmt.Printf("Execution time: %s\n", time.Since(start))
 }
